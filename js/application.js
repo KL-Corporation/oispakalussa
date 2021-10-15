e6d083951fb2bc4df2d754fdb4ad5a277ee9024f
NAL <niko.a.leinonen@gmail.com>
2021-10-15 20:38:33 +0300

    Fixed some urls

--- js/application.js
+++ js/application.js
@@ -9,14 +9,14 @@ window.requestAnimationFrame(function () {
         "https://www.oispakalussa.tk/img/4.png",
         "https://www.oispakalussa.tk/img/8.png",
         "https://www.oispakalussa.tk/img/16.png",
-        "http://www.oispakalussa.tk/img/32.png",
-        "http://www.oispakalussa.tk/img/64.png",
-        "http://www.oispakalussa.tk/img/128.png",
-        "http://www.oispakalussa.tk/img/256.png",
-        "http://www.oispakalussa.tk/img/512.png",
-        "http://www.oispakalussa.tk/img/1024.png",
+        "https://www.oispakalussa.tk/img/32.png",
+        "https://www.oispakalussa.tk/img/64.png",
+        "https://www.oispakalussa.tk/img/128.png",
+        "https://www.oispakalussa.tk/img/256.png",
+        "https://www.oispakalussa.tk/img/512.png",
+        "https://www.oispakalussa.tk/img/1024.png",
         "https://www.oispakalussa.tk/img/2048.png",
-	"http://www.oispakalussa.tk/img/katko.png"
+	"https://www.oispakalussa.tk/img/katko.png"
 
     ];
     for(var i = 0; i < imageList.length; i++ )
